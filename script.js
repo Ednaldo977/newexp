e05ce4358294782138bf2c42c204b03f22c0c884
Ednaldo Carlos <33844327+Ednaldo977@users.noreply.github.com>
2022-07-18 19:23:18 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,54 @@
+var cavalo = new Audio();
+cavalo.src = "sfx/cavalo.mp3";
+
+var tapa = new Audio();
+tapa.src = "sfx/tapa.mp3";
+
+var ai = new Audio();
+ai.src = "sfx/ai.mp3";
+
+var nao = new Audio();
+nao.src = "sfx/nao.mp3";
+
+var ui = new Audio();
+ui.src = "sfx/uuuuuuuuui.mp3";
+
+var demais = new Audio();
+demais.src = "sfx/demaaais.mp3";
+
+var elegosta = new Audio();
+elegosta.src = "sfx/elegosta.mp3";
+
+var danca = new Audio();
+danca.src = "sfx/danca.mp3";
+
+var ahuur = new Audio();
+ahuur.src = "sfx/ahuurr.mp3";
+
+var ai2 = new Audio();
+ai2.src = "sfx/ai2.mp3";
+
+var queisso = new Audio();
+queisso.src = "sfx/queisso.mp3";
+
+var chega = new Audio();
+chega.src = "sfx/cheeeega.mp3";
+
+var xi = new Audio();
+xi.src = "sfx/xiiiii.mp3";
+
+var eh = new Audio();
+eh.src = "sfx/eh.mp3";
+
+var maque = new Audio();
+maque.src = "sfx/maque.mp3";
+
+var quep = new Audio();
+quep.src = "sfx/quep.mp3";
+
+var okay = new Audio();
+okay.src = "sfx/okay.mp3";
+
+var pare = new Audio();
+pare.src = "sfx/pare.mp3";
+
